1b3acebeba136fba5820ccde68db3a1fb73ceb79
Joel Scoble <joel.scoble@outlook.com>
2015-09-20 20:48:15 +0000

    refactor queue to use append, only set cap on initialization, simplify queue structure and rely on built-ins more. Also bounded

--- queue.go
+++ queue.go
@@ -1,41 +1,19 @@
-// Queue implements a dynamic queue that will grow as needed, minimize
-// unnecessary growth, and can be safely accessed from multiple routines.
+// Queue implements a queue that can either be bounded or unbounded.
+// Allocations are minimized and the queue is concurrency safe.
 //
-// The queue can be configured with a max capacity, which will cap the size of
-// the queue so unbounded grouwth cannot occur.  When a maximum capacity for
-// a queue is defined, an error will occur if the queue is full and an attempt
-// is made to add another item to the queue.  If the tail of the capped queue
-// is at the cap, an attempt will be made to shift the queue towards the
-// beginning of the slice. If the queue cannot be shifted (it is full) an
-// error will be returned.
-//
-// A queue is created with a minimum length and an optional maximum size
-// (capacity).  If the max size of the queue == 0, the queue will be unbounded.
-// The growth rate of the queue is similar to that of a slice. When a queue
-// grows, all items in the queue are shifted so that the head of the queue
-// points to the first element in the queue.
-//
-// TODO: rewrite the following for clarity
-// Before an item is enqueued, the queue is checked to see if thie new item
-// will cause it to grow. If the tail == length, growth may occur. If the
-// head of the queue is past a certain point in the queue, which is currently
-// calculated using a percentage, the items in the queue will be shifted to start at
-// the beginning of the slice, instead of growing the slice. The queue's head and tail
-// will then be updated to reflect the shift.
+// On queue creation, the initial capacity will be equal to the received size.
+// If the 'bounded' bool is true, it will be a bounded queue, otherwise it can
+// grow as needed.
 //
 // After dequeuing an item, the head position will be checked. If the queue
-// io empty, head > tail, head and tail will be set to 0. This allows for
-// efficient reuse of the queue without having to check to see if the queue
-// items should be shifted or the queue should be grown. The contents of the
-// queue are not zeroed out.
+// is empty, head > tail, the queue will be reset.
 //
-// Once a queue grows, it will not be shrunk. This behavior may change in the
-// future.
+// Once a queue grows, it will not shrink.
 //
-// All exported methods on the queue use locking so that the queue is safe for
-// concurrency.  Unexported methods do not do any locking/unlocking since it
-// is expected that the calling method has already obtained the lock and will
-// release it as appropriate.
+// If a bounded queue is at its capacity, a check will be done to see if there
+// is any space at the front of the queue: if items have been dequeued. If
+// there are, the items will be shifted to make room for the new item, which
+// will then be enqueued. If the bounded queue is full, an error will occur.
 package dq
 
 import (
@@ -49,27 +27,26 @@ import (
 // be shifted to make room; otherwise the queue will grow
 var shiftPercent = 50
 
-// Queue represents a queue and everything needed to manage it. The preferred method
-// for creating a new Queue is to use the New() func.
+// Queue represents a queue and everything needed to manage it. The preferred
+// method for creating a new Queue is to use either NewQ() or NewQueue().
 type Queue struct {
 	sync.RWMutex
 	items        []interface{}
 	head         int // current item in queue
-	tail         int // tail is the next insert point. last item is tail - 1
-	maxCap       int // if > 0, the queue's cap cannot grow beyond this value
+	bounded      bool // whether or not this is a bounded queue
 	shiftPercent int // the % of items that need to be removed before shifting occurs
 }
 
 // NewQ returns an empty queue with a capacity equal to the recieved size value. If
 // maxCap is > 0, the queue will not grow larger than maxCap; if it is at maxCap
 // and growth is requred to enqueue an item, an error will occur.
-func NewQ(size, maxCap int) *Queue {
-	return &Queue{items: make([]interface{}, size, size), maxCap: maxCap, shiftPercent: shiftPercent}
+func NewQ(size int, bounded bool) *Queue {
+	return &Queue{items: make([]interface{}, 0, size), bounded: bounded, shiftPercent: shiftPercent}
 }
 
 // NewQueue is a convenience wrapper to NewQ().
-func NewQueue(size, maxCap int) *Queue {
-	return NewQ(size, maxCap)
+func NewQueue(size int, bounded bool) *Queue {
+	return NewQ(size, bounded)
 }
 
 // SetShiftPercent sets the queue's shiftPercent: the percentage of the queue
@@ -91,31 +68,24 @@ func (q *Queue) SetShiftPercent(i int) {
 }
 
 // Enqueue: adds an item to the queue. If adding the item requires growing
-// the queue, the queue will either be shifted, to make room at the end of the queue
-// or it will grow. If the queue cannot be grown, an error will be returned.
+// the queue, the queue will either be shifted, to make room at the end of
+// the queue, or it will grow.
+//
+// If the queue is a bounded queue and is full, an error will be returned.
 func (q *Queue) Enqueue(item interface{}) error {
 	q.Lock()
 	defer q.Unlock()
 	// See if it needs to grow
-	if q.tail == cap(q.items) {
+	if len(q.items) == cap(q.items) {
 		shifted := q.shift()
-		// if we weren't able to make room by shifting, grow the queue/
+		// if we weren't able to make room by shifting, grow the queue
 		if !shifted {
-			if q.maxCap > 0 && cap(q.items) == q.maxCap {
-				return fmt.Errorf("cannot enqueue '%v' to a capped queue at capacity", item)
-			}
-			err := q.grow()
-			if err != nil {
-				return err
+			if q.bounded {
+				return fmt.Errorf("bounded queue full: cannot enqueue '%v'", item)
 			}
 		}
 	}
-	if q.tail > len(q.items)  - 1 {
-		q.items = append(q.items, item)
-	} else {
-		q.items[q.tail] = item
-	}
-	q.tail++
+	q.items = append(q.items, item)
 	return nil
 }
 
@@ -124,12 +94,12 @@ func (q *Queue) Enqueue(item interface{}) error {
 func (q *Queue) Dequeue() interface{} {
 	q.Lock()
 	i := q.items[q.head]
-	q.head++
-	if q.head > q.tail {
+	if q.head == len(q.items) {
 		q.Unlock()
 		q.Reset()
 		return i
 	}
+	q.head++
 	q.Unlock()
 	return i
 }
@@ -137,7 +107,7 @@ func (q *Queue) Dequeue() interface{} {
 // IsEmpty returns whether or not the queue is empty
 func (q *Queue) IsEmpty() bool {
 	q.RLock()
-	if q.tail == 0 || q.head == q.tail {
+	if len(q.items) == 0  {
 		q.RUnlock()
 		return true
 	}
@@ -145,93 +115,39 @@ func (q *Queue) IsEmpty() bool {
 	return false
 }
 
-// Tail returns the current tail position
-func (q *Queue) Tail() int {
+// Count returns the current number of items in the queue
+func (q *Queue) Count() int {
 	q.RLock()
 	defer q.RUnlock()
-	return q.tail
-}
-
-// Head returns the current head position
-func (q *Queue) Head() int {
-	q.RLock()
-	defer	q.RUnlock()
-	return q.head
-}
-
-// Length returns the current length(cap) of the queue. Note, this is not the
-// number of items in the queue, for that use ItemCount()
-func (q *Queue) Length() int {
-	q.RLock()
-	defer q.RUnlock()
-	return cap(q.items)
-}
-
-// ItemCount returns the current number of items in the queue
-func (q *Queue) ItemCount() int {
-	q.RLock()
-	defer q.RUnlock()
-	return q.tail - q.head
+	return len(q.items) - q.head
 }
 
 // shift: if either shiftPercent items have been removed from the queue or the
-// queue is a capped queue, the remaining items in the queue will be shifted
+// queue is a boudned queue, the remaining items in the queue will be shifted
 // to the beginning of the queue. Returns whether or not a shift occurred
 func (q *Queue) shift() bool {
-	// shift percent applies to non-capped queues.
-	if q.maxCap == 0 {
+	// shift percent applies to unbounded queues.
+	if !q.bounded {
 		if q.head < (cap(q.items)*q.shiftPercent)/100 {
 			return false
 		}
 	}
 	// if this is a capped queue, but there is no room for a shift, nothing to do.
-	if q.maxCap > 0 {
+	if q.bounded {
 		if q.head == 0 {
 			return false
 		}
 	}
-	q.items = append(q.items[:0], q.items[q.head:q.tail]...)
+	q.items = append(q.items[:0], q.items[q.head:]...)
 	// set the pointers to the correct position
-	q.tail = q.tail - q.head
 	q.head = 0
 	return true
-}
-
-// grow grows the slice using an algorithm similar to growSlice(). This is a
-// bit slower than relying on slice's automatic growth, but allows for capacity
-// enforcement w/o growing the slice cap beyond the configured maxCap, if
-// applicable.
-//
-// Since a temporary slice is created to store the current queue, all items in
-// the queue are automatically shifted
-func (q *Queue) grow() error {
-	if cap(q.items) == q.maxCap && q.maxCap > 0 {
-		return fmt.Errorf("growQueue: cannot grow beyond max capacity of %d", q.maxCap)
-	}
-	var len int
-	if cap(q.items) < 1024 {
-		len = cap(q.items) << 1
-	} else {
-		len = cap(q.items) + cap(q.items)/4
-	}
-	// If the maxCap is set, cannot grow it beyond that
-	if len > q.maxCap && q.maxCap > 0 {
-		len = q.maxCap
-	}
-	// create a new slice of len
-	tmp := make([]interface{}, len, len)
-	// copy the remaining elements
-	copy(tmp, q.items[q.head:q.tail])
-	q.items = tmp
-	q.tail = q.tail - q.head
-	q.head = 0
-	return nil
 }
 
 // Reset resets the queue; head and tail point to element 0.
 func (q *Queue) Reset() {
 	q.Lock()
 	q.head = 0
-	q.tail = 0
+	q.items = q.items[:0]
 	q.Unlock()
 }
--- queue_test.go
+++ queue_test.go
@@ -4,20 +4,20 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	q := NewQ(10, 0)
+	q := NewQ(10, false)
 	if cap(q.items) != 10 {
 		t.Errorf("expected 10, got %d", cap(q.items))
 	}
-	if q.maxCap != 0 {
-		t.Errorf("expected 0, got %d", q.maxCap)
+	if q.bounded {
+		t.Errorf("expected false, got %t", q.bounded)
 	}
 
-	q = NewQueue(100, 200)
+	q = NewQueue(100, true)
 	if cap(q.items) != 100 {
 		t.Errorf("expected 100, got %d", cap(q.items))
 	}
-	if q.maxCap != 200 {
-		t.Errorf("expected 200, got %d", q.maxCap)
+	if !q.bounded {
+		t.Errorf("expected true, got %t", q.bounded)
 	}
 }
 
@@ -25,27 +25,27 @@ func TestNew(t *testing.T) {
 func TestQueueing(t *testing.T) {
 	var tests = []struct {
 		size        int
-		maxCap      int
+		bounded     bool
 		headPos     int
-		tailPos     int
+		expectedLen     int
 		expectedCap int
 		items       []interface{}
 		errString   string
 	}{
-		{size: 2, maxCap: 0, tailPos: 4, expectedCap: 4, items: []interface{}{0, 1, 2, 3}, errString: ""},
-		{size: 2, maxCap: 0, tailPos: 5, expectedCap: 8, items: []interface{}{0, 1, 2, 3, 4}, errString: ""},
-		{size: 2, maxCap: 4, tailPos: 4, expectedCap: 4, items: []interface{}{0, 1, 2, 3}, errString: ""},
+		{size: 2, bounded: false, expectedLen: 4, expectedCap: 4, items: []interface{}{0, 1, 2, 3}, errString: ""},
+		{size: 2, bounded: false, expectedLen: 5, expectedCap: 8, items: []interface{}{0, 1, 2, 3, 4}, errString: ""},
+		{size: 4, bounded: true, expectedLen: 4, expectedCap: 4, items: []interface{}{0, 1, 2, 3}, errString: ""},
 	}
 	for i, test := range tests {
-		q := NewQ(test.size, test.maxCap)
+		q := NewQ(test.size, test.bounded)
 		for _, v := range test.items {
 			_ = q.Enqueue(v)
 
 		}
 
 		// check that the items are as expected:
-		if len(q.items) != test.expectedCap {
-			t.Errorf("%d: expected %d items in queue, got %d", i, test.expectedCap, len(q.items))
+		if len(q.items) != test.expectedLen {
+			t.Errorf("%d: expected %d items in queue, got %d", i, test.expectedLen, len(q.items))
 		}
 		if cap(q.items) != test.expectedCap {
 			t.Errorf("%d: expected queue cap to be %d, got %d", i, test.expectedCap, cap(q.items))
@@ -53,13 +53,10 @@ func TestQueueing(t *testing.T) {
 		if q.head != test.headPos {
 			t.Errorf("%d: expected head to be at pos %d, got %d", i, test.headPos, q.head)
 		}
-		if q.maxCap != test.maxCap {
-			t.Errorf("%d: expected maxCap to be %d, was %d", i, test.maxCap, q.maxCap)
+		if q.bounded != test.bounded {
+			t.Errorf("%d: expected bounded to be %t, was %t", i, test.bounded, q.bounded)
 		}
-		if cap(q.items) > test.maxCap && test.maxCap > 0 {
-			t.Errorf("%d: expected cap of queue to be equal to it's max capacity, %d; was %d", i, test.maxCap, cap(q.items))
-		}
-		for j := 0; j < q.tail; j++ {
+		for j := 0; j < len(q.items); j++ {
 			if q.items[j] != test.items[j] {
 				t.Errorf("%d: expected value of index %d to be %d, got %d", i, j, test.items[j], q.items[j])
 			}
@@ -81,9 +78,9 @@ func TestQueueing(t *testing.T) {
 func TestDequeueEnqueue(t *testing.T) {
 	tests := []struct {
 		size        int
-		maxCap      int
+		bounded     bool
 		headPos     int
-		tailPos     int
+		expectedLen int
 		expectedCap int
 		dequeueCnt  int
 		dequeueVals []interface{}
@@ -91,14 +88,14 @@ func TestDequeueEnqueue(t *testing.T) {
 		items2      []interface{}
 		errString   string
 	}{
-		{size: 10, maxCap: 0, tailPos: 7, expectedCap: 10, dequeueCnt: 5, dequeueVals: []interface{}{0, 1, 2, 3, 4},
+		{size: 10, bounded: false, expectedLen: 7, expectedCap: 10, dequeueCnt: 5, dequeueVals: []interface{}{0, 1, 2, 3, 4},
 			items: []interface{}{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, items2: []interface{}{10, 11}, errString: ""},
 	}
 
 	// First add the queue
 	for _, test := range tests {
 		var err error
-		q := NewQ(test.size, test.maxCap)
+		q := NewQ(test.size, test.bounded)
 		for _, v := range test.items {
 			err = q.Enqueue(v)
 		}
@@ -131,8 +128,8 @@ func TestDequeueEnqueue(t *testing.T) {
 		if q.head != 0 {
 			t.Errorf("Expected head to be at pos 0, got %d", q.head)
 		}
-		if q.tail != test.tailPos {
-			t.Errorf("Expected tail to be at %d, got %d", test.tailPos, q.tail)
+		if len(q.items) != test.expectedLen {
+			t.Errorf("Expected tail to be at %d, got %d", test.expectedLen, len(q.items))
 		}
 		if cap(q.items) != test.expectedCap {
 			t.Errorf("Expected cap of queue to be %d. got %d", test.expectedCap, cap(q.items))
@@ -154,7 +151,7 @@ func TestSetShiftPercentage(t *testing.T) {
 		{100, 100},
 		{101, 100},
 	}
-	q := NewQueue(10, 0)
+	q := NewQueue(10, true)
 	for i, test := range tests {
 		q.SetShiftPercent(test.percent)
 		if q.shiftPercent != test.expected {
@@ -164,7 +161,7 @@ func TestSetShiftPercentage(t *testing.T) {
 }
 
 func TestCappedQueue(t *testing.T) {
-	q := NewQ(4, 4)
+	q := NewQ(4, true)
 	for i := 0; i < 4; i++ {
 		q.Enqueue(i)
 	}
@@ -172,17 +169,17 @@ func TestCappedQueue(t *testing.T) {
 	_ = q.Dequeue()
 	err := q.Enqueue(5)
 	if err != nil {
-		t.Errorf("Expected enqueue to a capped queue with tail at end, but room to shift to succeed; got %s", err)
+		t.Errorf("Expected enqueue to a bounded queue with len == cap, but room to shift to succeed; got %s", err)
 		return
 	}
 	// enqueue another item, queue is full, this should fail
 	err = q.Enqueue(6)
 	if err == nil {
-		t.Errorf("Expected enqueue to a capped queue that is full to error, it did not")
+		t.Errorf("Expected enqueue to a bounded queue that is full to error, it did not")
 		return
 	}
-	if err.Error() != "cannot enqueue '6' to a capped queue at capacity" {
-		t.Errorf("Expected enqueue to a capped queue to error with \"cannot enqueue '6' to a capped queue at capacity\" , got %q", err)
+	if err.Error() != "bounded queue full: cannot enqueue '6'" {
+		t.Errorf("Expected enqueue to a capped queue to error with \"bounded queue full: cannot enqueue '6'\" , got %q", err)
 	}
 
 }
