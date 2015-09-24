9d0d1762076e4302971f8774cc2515f61e6d59fa
Joel Scoble <joel.scoble@outlook.com>
2015-09-24 03:25:05 +0000

    add ring buffer implementation

---
+++ buffer/ring.go
@@ -0,0 +1,40 @@
+package buffer
+
+import (
+  "math"
+
+  "github.com/mohae/dq/queue"
+)
+
+// Ring is a ring buffer implementation wrapping queue.Circular.
+type Ring struct {
+  queue.Circular
+}
+
+// NewRing returns a ring buffer initalized with 'size' slots.
+func NewRing(size int) *Ring {
+  return &Ring{*queue.NewCircularQ(size)}
+}
+
+// Enqueue enques an item, If the buffer is full, the oldest item will
+// be evicted.
+func (r *Ring) Enqueue(item interface{}) error {
+  r.Lock()
+  // if the buffer is full, move the head forward
+  if r.isFull() {
+    r.Head = int(math.Mod(float64(r.Head + 1), float64(cap(r.Items))))
+  }
+  r.Items[r.Tail] = item
+  r.Tail = int(math.Mod(float64(r.Tail + 1), float64(cap(r.Items))))
+  r.Unlock()
+  return nil
+}
+
+// isFull is an unexported version that expects the caller to handle locking.
+// This eliminates double locking on enqueue
+func (r *Ring) isFull() bool {
+  if r.Head != int(math.Mod(float64(r.Tail + 1), float64(cap(r.Items)))) {
+    return false
+  }
+  return true
+}
