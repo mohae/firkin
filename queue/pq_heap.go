dbf283a04c4d40728bfa4a5491ba9b2adc5779d0
Joel Scoble <joel.scoble@outlook.com>
2015-10-23 02:40:02 +0000

    add heap based priority queue

---
+++ queue/pq_heap.go
@@ -0,0 +1,114 @@
+// Copyright 2015 by Joel Scoble
+//
+// This implementation of a priority queue using a heap is based on the code
+// provided in https://http://golang.org/src/container/heap/example_pq_test.go
+//
+// This implemantion uses interface{} and is thread safe.
+//
+// The original copyright notice:
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package queue
+
+import (
+	"container/heap"
+	"sync"
+)
+
+// An Item is something we manage in a priority queue.
+type Item struct {
+	value    interface{} // The value of the item; arbitrary.
+	priority int         // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A HeapPriority implements heap.Interface and holds Items.
+type HeapPriority struct {
+	mu    sync.Mutex
+	items PQueue
+}
+
+type PQueue []*Item
+
+func (pq PQueue) Len() int { return len(pq) }
+
+func (pq PQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and vlaue of an Item in the queue.
+func (pq *PQueue) update(item *Item, value string, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+
+// Returns a new priority queue with the item's cap set at l; if l > 0.
+func NewHeapPriority(l int) *HeapPriority {
+	if l <= 0 {
+		return &HeapPriority{}
+	}
+	return &HeapPriority{items: make([]*Item, l, l)}
+}
+
+func (pq HeapPriority) Len() int {
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
+	return pq.items.Len()
+}
+
+func (pq HeapPriority) Less(i, j int) bool {
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq.items.Less(i, j)
+}
+
+func (pq HeapPriority) Swap(i, j int) {
+	pq.mu.Lock()
+	pq.items.Swap(i, j)
+	pq.mu.Unlock()
+}
+
+func (pq *HeapPriority) Push(x interface{}) {
+	pq.mu.Lock()
+	pq.items.Push(x)
+	pq.mu.Unlock()
+}
+
+func (pq *HeapPriority) Pop() interface{} {
+	pq.mu.Lock()
+	defer pq.mu.Unlock()
+	return pq.items.Pop()
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *HeapPriority) update(item *Item, value string, priority int) {
+	pq.mu.Lock()
+	pq.items.update(item, value, priority)
+	pq.mu.Unlock()
+}
---
+++ queue/pq_heap_test.go
@@ -0,0 +1,65 @@
+// Copyright 2015 by Joel Scoble
+//
+// This implementation of a priority queue using a heap is based on the code
+// provided in https://http://golang.org/src/container/heap/example_pq_test.go
+//
+// This implemantion uses interface{} and is thread safe.
+//
+// The original copyright notice:
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package queue
+
+import (
+	"container/heap"
+	"testing"
+)
+
+func TestPQHeap(t *testing.T) {
+	// Some items and their priorities.
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := NewHeapPriority(len(items))
+	i := 0
+	for value, priority := range items {
+		pq.items[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+	heap.Init(&pq.items)
+
+	// Insert a new item and then modify its priority.
+	item := &Item{
+		value:    "orange",
+		priority: 1,
+	}
+	heap.Push(&pq.items, item)
+	pq.update(item, "grapefruit", 5)
+
+	// Take the items out; they arrive in decreasing priority order.
+	expected := []struct {
+		priority int
+		value string
+	}{
+		{5, "grapefruit"},
+		{4, "pear"},
+		{3, "banana"},
+		{2, "apple"},
+	}
+	i = 0
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq.items).(*Item)
+		if item.priority != expected[i].priority || item.value != expected[i].value {
+				t.Errorf("Expected %v got %v", expected[i], item)
+		}
+		i++
+	}
+}
